070b29994365502f0a535a76c7ebe7ed6ff792ee
Constantinos Argyriou <rounnus@gmail.com>
2021-12-18 18:19:38 +0200

    Done fix

---
+++ smart_lights/SmartLights.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Smart_Assistant_Design.NewFolder
+{
+    public partial class SmartLights : Form
+    {
+        Chooseroom cs = new Chooseroom();
+        public SmartLights()
+        {
+            InitializeComponent();
+        }
+
+        private void chooseroom_Click(object sender, EventArgs e)
+        {
+            cs.Show();
+        }
+    }
+}
